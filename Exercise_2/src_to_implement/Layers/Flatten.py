5833475b1910d7452e58819772b36544699d7448
Maitreya L. <maitu.clever@gmail.com>
2024-12-09 16:14:08 +0100

    Flatten done

---
+++ Exercise_2/src_to_implement/Layers/Flatten.py
@@ -0,0 +1,16 @@
+""" Flatten Layer """
+# This layer reshapes the multidimensional input to a one dimensional feature vector
+
+from Exercise_2.src_to_implement.Layers.Base import BaseLayer
+
+class Flatten(BaseLayer):
+    def __init__(self):
+        super().__init__()
+        self.shape = None
+
+    def forward(self, input_tensor):
+        self.shape = input_tensor.shape
+        return input_tensor.reshape(self.shape[0], -1)
+        
+    def backward(self, error_tensor):
+        return error_tensor.reshape(self.shape)
